6a0513de77379d3925e48ff96856e592d3e2afd2
Anondo Ray <ajoysarker553@gmail.com>
2026-04-16 12:48:32 +0600

    dashbord routing done

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,11 @@
 import App from "@/App"; 
+import DashboardLayout from "@/components/layout/DashboardLayout";
+import AddTour from "@/pages/Admin/AddTour";
+import Analytics from "@/pages/Admin/Analytics";
 import Homepage from "@/pages/Homepage";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
+import booking from "@/pages/User/booking";
 import Verify from "@/pages/Verify"; 
 import { createBrowserRouter } from "react-router" 
 
@@ -15,6 +19,33 @@ export const router = createBrowserRouter([
         index: true,
       }, 
     ],
+  },
+  {
+    Component: DashboardLayout,
+    path: "/admin",
+    children: [
+     { 
+        Component: Analytics,
+        path: "analytics",
+      
+      },
+     { 
+        Component: AddTour,
+        path: "add-tour",
+      
+      },
+    ]
+  },
+  {
+    Component: DashboardLayout,
+    path: "/user",
+    children: [
+     { 
+        Component: booking,
+        path: "booking",
+      
+      }
+    ]
   },
    
   {
